c71ec7199654c5ffe3fc6f63e86152ab6bd54ccf
Evgeny Akabenko <danlimons322@gmail.com>
2026-05-20 15:10:48 +0300

    Removed NikNaks from config

--- lua/autorun/gcal_init.lua
+++ lua/autorun/gcal_init.lua
@@ -19,7 +19,6 @@ if CLIENT then
         ["3425927104"] = "Vmanip Base with knockdown",
         ["2844472642"] = "Vmanip +",
         ["3714993549"] = "Vmanip (Lite)",
-        ["2861839844"] = "NikNaks"
         -- ["1234567890"] = "Example Addon"
     }
 
